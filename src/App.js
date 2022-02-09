fbde822fc4bb05a44df512b62ee198052e9f9b4b
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-02-09 18:19:44 -0300

    Desafio2-lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import * as api from './services/api';
 import Home from './components/Home';
-import {BrowserRouter, Route} from 'react-router-dom'
 
 class App extends React.Component {
   // constructor() {
@@ -9,11 +9,11 @@ class App extends React.Component {
   // }
 
   render() {
-    api.getCategories().then(categories => { console.log(categories) })
-   return (
-     <div className="App">
+    api.getCategories().then((categories) => { console.log(categories); });
+    return (
+      <div className="App">
         <BrowserRouter>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={ Home } />
         </BrowserRouter>
       </div>
     );
