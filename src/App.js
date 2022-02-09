9d9d1d31523823492997513775157fc42aaa598d
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-02-09 17:52:17 -0300

    Desafio 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import * as api from './services/api';
+import Home from './components/Home';
+import {BrowserRouter, Route} from 'react-router-dom'
 
 class App extends React.Component {
   // constructor() {
@@ -7,10 +9,11 @@ class App extends React.Component {
   // }
 
   render() {
-    api.getCategories().then((categories) => { console.log(categories); });
     return (
       <div className="App">
-        <p>Oi</p>
+        <BrowserRouter>
+          <Route exact path="/" component={Home}/>
+        </BrowserRouter>
       </div>
     );
   }
